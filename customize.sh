3b1d1cb5d8c32f3d288c3ac2ec23dfaa68de9055
liubei66 <46340917+liubei66@users.noreply.github.com>
2020-07-02 16:57:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,11 +7,15 @@
 if [ -e openwrt/package/base-files/files/bin/config_generate ] ; then
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 fi
-# Modify the version number
+#2.Modify the version number
 if [ -e openwrt/package/lean/default-settings/files/zzz-default-settings ] ; then
 sed -i 's/OpenWrt/J.K.Peng build $(date "+%Y.%m.%d") @ OpenWrt/g' openwrt/package/lean/default-settings/files/zzz-default-settings
 fi
-# Modify default theme
+# 3.Modify default theme
 if [ -e openwrt/feeds/luci/collections/luci/Makefile ] ; then
 sed -i 's/luci-theme-bootstrap/luci-theme-netgear/g' openwrt/feeds/luci/collections/luci/Makefile
+fi
+#4.Modify the shell banner
+if [ -e openwrt/package/base-files/files/etc/banner ] ; then
+sed -i 's/HomeLede v2020.06.27 based on OpenWrt R20.6.18/J.K.Peng build base on OpenWrt/g' openwrt/package/base-files/files/etc/banner
 fi
