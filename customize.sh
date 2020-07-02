bc36e638e60caed4313060476184bc6c7b081aad
liubei66 <46340917+liubei66@users.noreply.github.com>
2020-07-02 11:26:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -4,7 +4,9 @@
 # Lisence: MIT
 
 #1. Modify default IP
+if [ -e openwrt/package/base-files/files/bin/config_generate ] ; then
 sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+fi
 # Modify the version number
 if [ -e openwrt/package/lean/default-settings/files/zzz-default-settings ] ; then
 sed -i 's/OpenWrt/J.K.Peng build $(date "+%Y.%m.%d") @ OpenWrt/g' openwrt/package/lean/default-settings/files/zzz-default-settings
