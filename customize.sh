1490b3bcb009c307c08fcbaf1cde27a275e6584b
liubei66 <46340917+liubei66@users.noreply.github.com>
2020-07-02 09:22:36 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
+# Modify the version number
+if [ -e openwrt/package/lean/default-settings/files/zzz-default-settings ] ; then
+sed -i 's/OpenWrt/J.K.Peng build $(date "+%Y.%m.%d") @ OpenWrt/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+fi
+# Modify default theme
+if [ -e openwrt/feeds/luci/collections/luci/Makefile ] ; then
+sed -i 's/luci-theme-bootstrap/luci-theme-netgear/g' openwrt/feeds/luci/collections/luci/Makefile
+fi
